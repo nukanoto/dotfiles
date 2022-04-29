f6ffc26cab7dcb865477b26f4c5febe4f9a82725
nukanoto <nukanoto@users.noreply.github.com>
2022-04-29 17:38:11 +0900

    fix: .config/ -> config/

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -6,10 +6,10 @@ function link_file () {
     echo -e "\e[31mError: There is a file with the same name ($2)\e[0m"
   fi
 
-  ln -sf $1 $2
+  ln -s $1 $2
 }
 
-link_file $DOTPATH/.config/* ~/.config/
+ln -s $DOTPATH/config/* ~/.config/
 link_file $DOTPATH/.gitconfig ~/.gitconfig
 link_file $DOTPATH/.gitignore_global ~/.gitignore_global
 link_file $DOTPATH/.config/zsh/.zshenv ~/.zshenv
