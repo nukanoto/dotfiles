3c8c71855359643b140020801c1e1b2d0a302b52
nukanoto <me@nukanoto.net>
2025-08-31 15:48:44 +0900

    fix(git): add ignores and use `main` as default branch

--- home/git.nix
+++ home/git.nix
@@ -5,5 +5,14 @@
     userName = "nukanoto";
     userEmail = "me@nukanoto.net";
     lfs.enable = true;
+    extraConfig = {
+      init.defaultBranch = "main";
+    };
+    ignores = [
+      "/.claude/"
+      "/CLAUDE.md"
+      ".envrc"
+      ".direnv/"
+    ];
   };
 }
